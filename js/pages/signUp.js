9d4aa8e30450d80896ebf65fbee0d065955af86f
Kyle0817 <gn02025752@gmail.com>
2021-01-23 16:54:20 +0800

    做好註冊功能需再次驗證

--- js/pages/signUp.js
+++ js/pages/signUp.js
@@ -5,7 +5,8 @@ const signUp = {
         <form id="signUpForm" 
         @submit.prevent="signUpSubmit">
           <div class="animate-label" v-for="label in signUpLabels" :key="label.id">
-            <input :type="label.type" :id="label.id" required autocomplete="off" v-model="label.tempData" />
+            <input :type="label.type" :id="label.id" required autocomplete="off" v-model="label.inputData" 
+              @change="verifyData(label)" />
             <label :for="label.id">{{label.title}}</label>
             <span></span>
             <p class="warning" v-if="!label.verifyInputData">{{label.warning}}</p>
@@ -21,32 +22,32 @@ const signUp = {
           title: "身份證字號",
           id: "userId",
           type: "text",
-          tempData: "",
-          warning:  "",             //"身份證字號重覆",
+          inputData: "",
+          warning:  "",
           verifyInputData: true
         },
         {
           title: "姓名",
           id: "username",
           type: "text",
-          tempData: "",
-          warning: "",             //"姓名不得包含特殊字元",
+          inputData: "",
+          warning: "",
           verifyInputData: true
         },
         {
           title: "密碼",
           id: "password",
           type: "password",
-          tempData: "",
-          warning: "",             //"密碼不得有符號",
+          inputData: "",
+          warning: "", 
           verifyInputData: true
         },
         {
           title: "確認密碼",
           id: "checkPassword",
           type: "password",
-          tempData: "",
-          warning: "",             //"密碼兩者不相同",
+          inputData: "",
+          warning: "",
           verifyInputData: true
         },
       ],
@@ -60,52 +61,40 @@ const signUp = {
   },
   methods: {
     signUpSubmit() {
-      var signUpArr = [];
-      this.signUpLabels.forEach(function (item) {
-        signUpArr.push(item.registerData);
-      });
       var signUpObj = this.signUpData;
-      var [customerId, password, userName, checkPassword] = signUpArr;
-      signUpObj.customerId = customerId;
-      signUpObj.userName = userName;
-      signUpObj.password = password;
-      signUpObj.checkPassword = checkPassword;
-      var member = JSON.parse(localStorage.getItem(customerId)) || [];
-      if (customerId !== member.customerId) {
-        localStorage.setItem(customerId, JSON.stringify(signUpObj));
-        alert("註冊成功");
-        // 登入成功後自動轉首頁
-        this.$router.push({ name: "home" });
-      }
+      var isMember = JSON.parse(localStorage.getItem(signUpObj.id)) || [];
+      if (signUpObj.id !== isMember.id && this.signUpLabels.every(e => e.warning === "")) {
+          localStorage.setItem(signUpObj.id, JSON.stringify(signUpObj));
+          alert("註冊成功");
+          this.$router.push({ name: "home" });
+        } else if(signUpObj.id === isMember.id) 
+            alert("此身份證字號已註冊")
+          else
+            alert("請填寫正確的資料格式");
     },
-  },
-  watch: {
-    signUpLabels: [{
-      handler: function(obj) {
-        console.log(obj)
-        let verifyId = /^[A-Z]{1}[1-2]{1}[0-9]{8}$/;
-        let verifyName = new RegExp("[\\u4e00-\\u9fa5]$");
-        // let verifyPassword = /^(?=.*?[a-z)(?=.*>[A-Z])(?=.*?[0-9])[a-zA_Z0-9]{6,10}$/;
-        let [id, userName, password, checkPassword] = obj;
-        // signUpData = obj;
-        // let [id, userName, password, checkPassword] = obj;
-        // console.log(verifyName.test(name.tempData))
-        // console.log(signUpData)
-        console.log(id)
-        console.log(userName)
-        console.log(password)
-        console.log(checkPassword)
-        // if(!verifyId.test(id.tempData)) {
-        //   id.verifyInputData = false;
-        //   id.warning = '身份證字號不得重覆或包含特殊字元';
-        // } else
-        //   id.warning = '';
-        
-      },
-      deep: true
+    verifyData(data) {
+      let verifyId = /^[A-Z]{1}[1-2]{1}[0-9]{8}$/;
+      let verifyName = /^[\u4e00-\u9fa5]{2,4}$/;
+      let verifyPassword = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*()_+`\-={}\[\]:";'<>?,.\/]).{6,12}$/;
+      if(data.id === 'userId') {
+          this.signUpData.id = data.inputData;
+          data.verifyInputData = false;
+          data.warning = !verifyId.test(data.inputData) ? "身份證字號格式錯誤" : "";
+      } else if(data.id === 'username') {
+          this.signUpData.userName = data.inputData;
+          data.verifyInputData = false;
+          data.warning = !verifyName.test(data.inputData) ? "請輸入中文字且長度限於2-4個字" : "";
+      } else if(data.id === 'password') {
+          this.signUpData.password = data.inputData;
+          data.verifyInputData = false;
+          data.warning = !verifyPassword.test(data.inputData) ? "密碼必須由6-12位字母、數字、特殊符號組成" : "";
+      } else if(data.id === 'checkPassword') {
+          this.signUpData.checkPassword = data.inputData;
+          data.verifyInputData = false;
+          data.warning = !(data.inputData === this.signUpData.password) ? "與上述密碼不相同" : "";
+      }
     }
-    ]
-  }
-};
+  },
+}
 
 export default signUp;
